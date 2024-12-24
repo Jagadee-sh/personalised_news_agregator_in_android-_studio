7c468b0762588332ff933db37d6216b64fa4e96d
Jagadee-sh <jeevannobel@gmail.com>
2024-12-24 07:24:30 +0530

    Create News item.java

---
+++ News item.java
@@ -0,0 +1,26 @@
+package com.example.newsaggregator;
+
+public class NewsItem {
+
+    private String title;
+    private String description;
+    private String imageUrl;
+
+    public NewsItem(String title, String description, String imageUrl) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
